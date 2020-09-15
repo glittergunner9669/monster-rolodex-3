c9f7b2bcc7079a59a779a3ab13285f6392d4daca
Mabunay <mabunaygo@MNLPREPDSN2WSA.local>
2020-09-15 23:57:39 +0800

    Updates

---
+++ precache-manifest.753be8344da4d2812bc0251f8b17a7fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d31a498adca0e15f83174787c2e492e",
+    "url": "/monsters-rolodex-3/index.html"
+  },
+  {
+    "revision": "4cfa35003a28164f3564",
+    "url": "/monsters-rolodex-3/static/css/main.618b9af6.chunk.css"
+  },
+  {
+    "revision": "be3b3773af843de42433",
+    "url": "/monsters-rolodex-3/static/js/2.e0474869.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-3/static/js/2.e0474869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cfa35003a28164f3564",
+    "url": "/monsters-rolodex-3/static/js/main.6ff78450.chunk.js"
+  },
+  {
+    "revision": "ea2caa088d0ab36495dd",
+    "url": "/monsters-rolodex-3/static/js/runtime-main.31e06f21.js"
+  }
+]);
